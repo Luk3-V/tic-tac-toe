1b2e9a5c3b05aba87c442b669bddbfcbf543f741
Luk3-V <luke.vanhaezebrouck@gmail.com>
2022-06-25 18:22:09 -0700

    add 2 player mode

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
+const scoreOne = document.querySelector('.scoreOne');
+const scoreTwo = document.querySelector('.scoreTwo');
 const boardDiv = document.querySelector('.board');
 const modal = document.querySelector('.modal');
-const resetBtn = document.querySelector('.resetBtn');
+const clearBtn = document.querySelector('.clearBtn');
 const newBtn = document.querySelector('.newBtn');
 const startBtn = document.querySelector('.startBtn');
 const closeBtn = document.querySelector('.closeBtn');
+const form = document.querySelector('form');
 
 const sizeInput = document.getElementById("size");
 const sizeValue = document.getElementById("sizeValue");
 
-const Player = (name, symbol) => {
-    let _name = name;
-    let _symbol = symbol;
-    let _score = 0;
-
-    const getName = () => {
-        return _name;
-    }
-    const getSymbol = () => {
-        return _symbol;
-    }
-    const getScore = () => {
-        return _score;
-    }
-    const addPoint = () => {
-        _score++;
-    }
-
-    return {getName, getSymbol, getScore, addPoint};
-}
+// ------------ OBJECTS -------------
 
 const gameBoard = (() => {
-    let matrix = [];
-    let matrixSize = 0;
+    let board = [];
+    let boardSize = 0;
 
     const create = (size) => {
         if(size < 3)
             return;
        
-        matrix = [];
-        matrixSize = size;
+        board = new Array(size*size);
+        boardSize = size;
 
         boardDiv.innerHTML = '';
         
         for(var i=0; i<size; i++){
-            matrix[i] = new Array(size);
             const row = document.createElement('div');
             row.classList.add('row');
             row.setAttribute('style', `height:${100/size}%;`);
@@ -51,41 +34,147 @@ const gameBoard = (() => {
                 const cell = document.createElement('div');
                 cell.classList.add('cell');
                 cell.setAttribute('style', `width:${100/size}%;`);
-                cell.id = j+i*size;  
+                cell.id = i*size+j;  
+                cell.onclick = gameController.playCell;
                 row.appendChild(cell);
             }
             boardDiv.appendChild(row);
+        } 
+    }
+
+    const update = (index, player) => {
+        board[index] = player;
+        const cell = document.getElementById(index);
+        if(player == 'X')
+            cell.innerHTML = `<i class="fa-solid fa-x" style="font-size:${500/boardSize}px;"></i>`;
+        else
+            cell.innerHTML = `<i class="fa-solid fa-o" style="font-size:${500/boardSize}px;"></i>`;
+    }
+
+    const clear = () => {
+        create(boardSize);
+    }
+
+    const getSize = () => {
+        return boardSize;
+    }
+
+    const isEmpty = (index) => {
+        return (board[index] == null);
+    }
+
+    const isWin = (index) => {
+        const row = Math.floor(index/boardSize);
+        const col = index%boardSize;
+        const player = board[index];
+
+        let horizontal = true;
+        let vertical = true;
+        let diagonal1 = true;
+        let diagonal2 = true;
+
+        for(let i=0; i<boardSize; i++){ // Horizontal
+            if(board[i + row*boardSize] != player){
+                horizontal = false;
+                break;
+            }   
         }
+        for(let i=0; i<(boardSize*boardSize); i+=eval(boardSize)){ // Vertical
+            if(board[i + col] != player){
+                vertical = false;
+                break;
+            }
+        }
+        for(let i=0; i<boardSize; i++){ // Diagonal 1
+            if(board[i*boardSize+i] != player){
+                diagonal1 = false;
+                break;
+            }
+        }
+        for(let i=0; i<boardSize; i++){ // Diagonal 2
+            if(board[i*boardSize+(boardSize-1-i)] != player){
+                diagonal2 = false;
+                break;
+            }
+        }
+
+        return horizontal || vertical || diagonal1 || diagonal2;
     }
 
-    const update = (x, y, player) => {
-        matrix[x][y] = player;
-        const cell = document.getElementById(x+y*matrixSize);
-        cell.innerHTML = player;
-    }
-
-    const reset = () => {
-        create(matrixSize);
-    }
-
-    return {create, update, reset};
+    return {create, update, clear, getSize, isEmpty, isWin};
 })();
 
 const gameController = (() => {
-    const start = () => {
-        console.log('start');
-        gameBoard.create(3);
+    let player1 = 0; // 'X'
+    let player2 = 0; // 'O'
+    let startTurn = 'X';
+    let currentTurn = 'X';
+    let playing = true;
+
+    const start = (size) => {     
+        player1 = 0;
+        player2 = 0;
+        updateScore();
+        startTurn = 'X';
+        setTurn('X');
+        playing = true;
+        gameBoard.create(size);
     }
 
-    const reset = () => {
-        console.log('reset');
-        gameBoard.reset();
+    const clear = () => {
+        if(startTurn == 'X'){
+            startTurn = 'O';
+            setTurn('O');
+        }
+        else {
+            startTurn = 'X';
+            setTurn('X');
+        }
+        playing = true;
+        gameBoard.clear();
     }
 
-    return {start, reset};
+    const setTurn = (player) => {
+        if(player == 'X'){
+            scoreTwo.setAttribute("style", "border: 1px solid darkgrey;");
+            scoreOne.setAttribute("style", "border: 1px solid red; transform: scale(1.1,1.1);");  
+        }
+        else {      
+            scoreOne.setAttribute("style", "border: 1px solid darkgrey;");
+            scoreTwo.setAttribute("style", "border: 1px solid dodgerblue; transform: scale(1.1,1.1);");       
+        }   
+        currentTurn = player;             
+    }
+
+    const playCell = (e) => {
+        if(playing && gameBoard.isEmpty(e.currentTarget.id)){
+            gameBoard.update(e.currentTarget.id, currentTurn);
+            if(gameBoard.isWin(e.currentTarget.id)){
+                console.log('win');
+                if(currentTurn == 'X')
+                    player1++;
+                else
+                    player2++;
+                updateScore();
+                playing = false;
+                return;
+            }
+            if(currentTurn == 'X')
+                setTurn('O');
+            else 
+                setTurn('X');               
+        }   
+    }
+
+    const updateScore = () =>{
+        scoreOne.lastElementChild.innerHTML = player1;
+        scoreTwo.lastElementChild.innerHTML = player2;
+    }
+
+    return {start, clear, playCell};
 })();
 
-// -------------------------------------
+// ------------ FUNCTIONS -------------
 
 const openModal = (e) => {
     modal.style.display = "flex";
@@ -94,13 +183,14 @@ const closeModal = () => {
     modal.style.display = "none";
 }
 const startGame = (e) => {
-    gameController.start();
+    e.preventDefault();
+    gameController.start(form.size.value);
+    closeModal();
 }
-
 
 // ------------- EVENTS ----------------
 
-resetBtn.onclick = gameController.reset;
+clearBtn.onclick = gameController.clear;
 newBtn.onclick = openModal;
 closeBtn.onclick = closeModal;
 startBtn.onclick = startGame;
@@ -112,21 +202,15 @@ sizeInput.oninput = () => {
 // --------------- INIT ----------------
 
 sizeValue.innerHTML = sizeInput.value;
-gameController.start();
+gameController.start(3);
 
-// gameboard object
-// - set size X
-// - reset X
-// - update X
-// - display X
-// - store positions X
 
-// player object
-// - x or o X
-// - win counter X
 
-// game controller
-// - start
-// - track turns
-// - track moves, prevent illegal moves
-// - detect win, update score, and reset game
+
+
+
+// TODO
+// - add bot gamemode
+// - add win indicator (highlight winning squares)
+// - add draw indicator
+// - initial start menu
